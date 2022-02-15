5d852e2167d49da1c851f44aece2b5cae096c54c
Ugur Selim Ozen <ugur_ozen58@hotmail.com>
2022-02-16 00:04:38 +0300

    Update footballPlayerRankingApp.py

--- Project3_Classification_SQL/FootballPlayerRankingApp/footballPlayerRankingApp.py
+++ Project3_Classification_SQL/FootballPlayerRankingApp/footballPlayerRankingApp.py
@@ -4,25 +4,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-from sklearn.model_selection import train_test_split
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from catboost import CatBoostClassifier, Pool
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.model_selection import train_test_split ,cross_val_score , KFold
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import r2_score
+from catboost import CatBoostClassifier
+from sklearn.preprocessing import StandardScaler
 import sklearn.metrics as metrics
 import streamlit as st
 from bokeh.models.widgets import Div
